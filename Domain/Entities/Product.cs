0672237d7bde4da1dd516c7448be29b88c63019c
ArashNabaei <Arash_ANB@yahoo.com>
2025-11-29 18:46:02 +0330

    Add Product model

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Domain.Entities
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ProduceDate { get; set; }
+
+        public string ManufacturePhone { get; set; }
+
+        public string ManufactureEmail { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+    }
+}
