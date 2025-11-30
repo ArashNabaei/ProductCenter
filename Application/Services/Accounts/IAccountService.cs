dbb1210f5552e846ce4a1034dec6bb9823fcdb14
ArashNabaei <Arash_ANB@yahoo.com>
2025-11-30 23:07:54 +0330

    Add  IAccountService and its methods

---
+++ Application/Services/Accounts/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿
+using Application.Dtos;
+
+namespace Application.Services.Accounts
+{
+    public interface IAccountService
+    {
+
+        Task CreateUser(AccountDto user);
+
+        string GenerateToken(int userId);
+
+        Task<int> ValidateUser(string username, string password);
+
+    }
+}
