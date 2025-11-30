9c14e0e7d53b1f89c7a9ab31bdff8f624d27417f
ArashNabaei <Arash_ANB@yahoo.com>
2025-11-30 11:47:42 +0330

    Add EfContext class

---
+++ Infrastructure/EfContext.cs
@@ -0,0 +1,25 @@
+﻿
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class EfContext : DbContext
+    {
+        
+        public EfContext(DbContextOptions<EfContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
