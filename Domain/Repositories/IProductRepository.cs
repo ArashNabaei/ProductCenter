61cab3e6aad298be464eef3e6930fa6598fa13f8
ArashNabaei <Arash_ANB@yahoo.com>
2025-11-30 06:36:31 +0330

    Create and implement IProductRepository interface

---
+++ Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IProductRepository
+    {
+
+        Task<Product> GetProductById(int userId,  int productId);
+
+        Task<List<Product>> GetProducts(int userId);
+
+        Task CreateProduct(int userId);
+
+        Task DeleteProduct(int userId, int productId);
+
+        Task UpdateProduct(int userId, int productId, Product product);
+
+    }
+}
