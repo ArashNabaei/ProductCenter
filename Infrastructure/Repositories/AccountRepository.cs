2451b05e80d7367872612850729cd1b4221ee90d
ArashNabaei <Arash_ANB@yahoo.com>
2025-11-30 12:27:16 +0330

    Implement AccountRepository

---
+++ Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,38 @@
+﻿
+using Domain.Entities;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+
+        private readonly EfContext _efcontext;
+
+        public AccountRepository(EfContext efContext)
+        {
+            _efcontext = efContext;
+        }
+
+        public async Task CreateUser(string username, string password)
+        {
+            var user = new User
+            {
+                Username = username,
+                Password = password
+            };
+
+            await _efcontext.Users.AddAsync(user);
+            await _efcontext.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetUserByUsernameAndPassword(string username, string password)
+        {
+            var user = await _efcontext.Users.FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+
+            return user;
+        }
+
+    }
+}
