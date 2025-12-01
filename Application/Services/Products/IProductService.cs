38a2cf8ad47afd07fb1a71e6a45875729c25b8af
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-01 08:09:56 +0330

    Implement IProductService

--- Application/Services/Products/IProductService.cs
+++ Application/Services/Products/IProductService.cs
@@ -1,7 +1,24 @@
 ﻿
+using Application.Dtos;
+
 namespace Application.Services.Products
 {
     public interface IProductService
     {
+
+        Task<ProductDto> GetProductById(int userId, int productId);
+
+        Task<List<ProductDto>> GetProducts(int userId);
+
+        List<ProductDto> GetProducts();
+
+        Task<List<ProductDto>> GetProductsFilteredByUser(int userId);
+
+        Task CreateProduct(int userId, string name);
+
+        Task DeleteProduct(int userId, int productId);
+
+        Task UpdateProduct(int userId, int productId, ProductDto productDto);
+
     }
 }
