1e65526d296c01f98576a98cb8490537d06de694
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-01 22:38:33 +0330

    Create validator for SignUpCommand

---
+++ Application/Features/Users/Commands/SignUpCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿
+using FluentValidation;
+
+namespace Application.Features.Users.Commands
+{
+    public class SignUpCommandValidator : AbstractValidator<SignUpCommand>
+    {
+
+        public SignUpCommandValidator()
+        {
+            RuleFor(c => c.Username)
+                .NotEmpty().WithMessage("Username is required.");
+
+            RuleFor(c => c.Password)
+                .NotEmpty().WithMessage("Password is required.");
+
+        }
+
+    }
+}
