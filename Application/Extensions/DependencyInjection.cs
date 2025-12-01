520d8a33923980b941d71bcfd869e4c280c18960
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-01 17:19:26 +0330

    Create new directory for extensions Add and implement DependencyInjection class

---
+++ Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿
+using Application.Services.Accounts;
+using Application.Services.Products;
+using Domain.Repositories;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<EfContext>();
+
+            return services;
+        }
+
+    }
+}
