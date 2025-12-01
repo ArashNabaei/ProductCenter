b720212744b999d46ad6dad7a854bb533b07e141
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-01 08:00:53 +0330

    Create ProductDto

---
+++ Application/Dtos/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Application.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ProduceDate { get; set; }
+
+        public string ManufacturePhone { get; set; }
+
+        public string ManufactureEmail { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+    }
+}
