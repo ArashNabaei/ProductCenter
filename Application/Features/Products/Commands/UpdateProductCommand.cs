a631438fa89bd23bd07259eff6d46771b8f12d33
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-01 17:09:09 +0330

    Add and Implement UpdateProductCommand

---
+++ Application/Features/Products/Commands/UpdateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿
+using Application.Dtos;
+using MediatR;
+
+namespace Application.Features.Products.Commands
+{
+    public record UpdateProductCommand(int UserId, int ProductId, ProductDto ProductDto) : IRequest
+    {
+    }
+}
