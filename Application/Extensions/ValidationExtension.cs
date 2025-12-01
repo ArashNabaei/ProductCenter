a8fc93b8b3e24ca8f95a8049d8d884f4439efc62
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-01 22:41:03 +0330

    Create a new extension class for validators Add SignUpCommandValidator to extension method

---
+++ Application/Extensions/ValidationExtension.cs
@@ -0,0 +1,19 @@
+﻿
+using Application.Features.Users.Commands;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class ValidationExtension
+    {
+
+        public static IServiceCollection AddValidators(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<SignUpCommand>, SignUpCommandValidator>();
+
+            return services;
+        }
+
+    }
+}
