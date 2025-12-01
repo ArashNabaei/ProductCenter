1c68ee63890e8022ef31011e2441471decf26526
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-01 17:11:41 +0330

    Create and Implement UpdateProductCommandHandler

---
+++ Application/Features/Products/Commands/UpdateProductCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using Application.Services.Products;
+using MediatR;
+
+namespace Application.Features.Products.Commands
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand>
+    {
+
+        private readonly IProductService _productService;
+
+        public UpdateProductCommandHandler(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task Handle (UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            await _productService.UpdateProduct(request.UserId, request.ProductId, request.ProductDto);
+        }
+
+    }
+}
