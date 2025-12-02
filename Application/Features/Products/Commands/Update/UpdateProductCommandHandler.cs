f21ddbe63a047fe2196d61241342ffcd4b72ec2a
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-02 07:53:56 +0330

    Run validation of the Update command for products in the handler

--- Application/Features/Products/Commands/Update/UpdateProductCommandHandler.cs
+++ Application/Features/Products/Commands/Update/UpdateProductCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Services.Products;
+using FluentValidation;
 using MediatR;
 
 namespace Application.Features.Products.Commands.Update
@@ -8,13 +9,21 @@ namespace Application.Features.Products.Commands.Update
 
         private readonly IProductService _productService;
 
-        public UpdateProductCommandHandler(IProductService productService)
+        private readonly IValidator<UpdateProductCommand> _validator;
+
+        public UpdateProductCommandHandler(IProductService productService, IValidator<UpdateProductCommand> validator)
         {
             _productService = productService;
+            _validator = validator;
         }
 
         public async Task Handle(UpdateProductCommand request, CancellationToken cancellationToken)
         {
+            var validationResult = await _validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
             await _productService.UpdateProduct(request.UserId, request.ProductId, request.ProductDto);
         }
 
