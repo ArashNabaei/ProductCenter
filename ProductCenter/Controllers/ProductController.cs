b71fc66104b179c1dd08e76381b9da7053a3f049
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-02 09:11:28 +0330

    Add ProductController Implement GetProductById API

---
+++ ProductCenter/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Products.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCenter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : BaseController
+    {
+
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductById(int productId)
+        {
+            var result = await _mediator.Send(new GetProductByIdQuery(UserId, productId));
+
+            return Ok(result);
+        }
+
+    }
+}
