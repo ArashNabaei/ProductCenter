5d16e1c5b6faaa18cf1dec1ce026e299cf4e87f0
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-02 09:04:23 +0330

    Create AccountController Implement SignUp API

---
+++ ProductCenter/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Application.Features.Users.Commands;
+
+namespace ProductCenter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : BaseController
+    {
+
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp(SignUpCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("User registered successfully.");
+        }
+
+
+
+    }
+}
