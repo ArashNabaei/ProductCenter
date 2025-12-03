72b9092a0a187b9615274c0d407ba483fe293186
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-03 09:05:18 +0330

    Create ProductServiceTest class

---
+++ Test/Services/Products/ProductServiceTest.cs
@@ -0,0 +1,23 @@
+﻿
+using Application.Services.Products;
+using Infrastructure.Repositories;
+using Moq;
+
+namespace Test.Services.Products
+{
+    public class ProductServiceTest
+    {
+
+        private readonly Mock<ProductRepository> _productRepository;
+
+        private readonly IProductService _productService;
+
+        public ProductServiceTest()
+        {
+            _productRepository = new Mock<ProductRepository>();
+            _productService = new ProductService(_productRepository.Object,
+                null);
+        }
+
+    }
+}
