597361f93aac90ecd0073b0b941e15ace0805e63
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-03 08:43:19 +0330

    Add a test for ValidateUser

--- Test/Services/Accounts/AccountServiceTest.cs
+++ Test/Services/Accounts/AccountServiceTest.cs
@@ -1,7 +1,40 @@
 ﻿
+using Application.Services.Accounts;
+using Domain.Entities;
+using Domain.Repositories;
+using Moq;
+using Shared.Exceptions.Accounts;
+using Test.Mocks;
+
 namespace Test.Services.Accounts
 {
     public class AccountServiceTest
     {
+        private readonly Mock<IAccountRepository> _accountRepository;
+
+        private readonly IAccountService _accountService;
+
+        public AccountServiceTest()
+        {
+            _accountRepository = new Mock<IAccountRepository>();
+
+            _accountService = new AccountService(
+                _accountRepository.Object,
+                null, null);
+        }
+
+        [Fact]
+        public async Task ValidateUser_WithInvalidCredentials_ShouldThrowsUserNotFoundException()
+        {
+            var user = AccountMocks.InvalidUser();
+
+            _accountRepository.Setup(r => r.GetUserByUsernameAndPassword(user.Username, user.Password))
+                              .ReturnsAsync((User?)null);
+
+            var exception = await Assert.ThrowsAsync<AccountException>(() => _accountService.ValidateUser(user.Username, user.Password));
+
+            Assert.Equal(1002, exception.Code);
+        }
+
     }
 }
