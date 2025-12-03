3e3334fb20aba2e7de6941c33f507fe02f45c173
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-03 10:35:24 +0330

    Fix validation of UpdateProductCommand

--- Application/Features/Products/Commands/Update/UpdateProductCommandValidator.cs
+++ Application/Features/Products/Commands/Update/UpdateProductCommandValidator.cs
@@ -9,12 +9,18 @@ namespace Application.Features.Products.Commands.Update
         public UpdateProductCommandValidator()
         {
 
-            RuleFor(c => c.ProductId)
-                .NotEmpty().WithMessage("Product id is required.")
-                .GreaterThan(0).WithMessage("Product is must be greater than 0.");
-
             RuleFor(c => c.ProductDto.Name)
                 .NotEmpty().WithMessage("Product name is required");
+
+            RuleFor(c => c.ProductDto.Id)
+                .NotEmpty().WithMessage("Product name is required")
+                .GreaterThan(0).WithMessage("Product id must be greater than 0.");
+
+            RuleFor(c => c.ProductId)
+                .NotEmpty().WithMessage("Product id is required.")
+                .GreaterThan(0).WithMessage("Product id must be greater than 0.")
+                .Must((command, productId) => productId == command.ProductDto.Id)
+                .WithMessage("The Ids should be equal.");
 
         }
 
