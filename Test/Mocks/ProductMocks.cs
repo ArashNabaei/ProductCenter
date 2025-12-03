57843e44dfdeaa27d7fc197a624ae0a4d6cd6727
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-03 08:54:00 +0330

    Create ProductMocks file Add three mocks in product mocks class

---
+++ Test/Mocks/ProductMocks.cs
@@ -0,0 +1,37 @@
+﻿
+using Domain.Entities;
+
+namespace Test.Mocks
+{
+    public static class ProductMocks
+    {
+
+        public static Product ValidProduct()
+        {
+            return new Product
+            {
+                Id = 1,
+                Name = "ValidProductName",
+            };
+        }
+
+        public static Product InvalidProduct()
+        {
+            return new Product
+            {
+                Id = 1,
+                Name = "InvalidProductName",
+            };
+        }
+
+        public static Product ExistingProduct()
+        {
+            return new Product
+            {
+                Id = 1,
+                Name = "ExistingProductName",
+            };
+        }
+
+    }
+}
