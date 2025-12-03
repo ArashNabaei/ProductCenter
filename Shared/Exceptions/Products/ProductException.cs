55bd86ca89235066eab1e303db2d4577bfb18775
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-03 08:13:19 +0330

    Create new directory for product exceptions Add new class as ProductException Implement ProductNotFoundException

---
+++ Shared/Exceptions/Products/ProductException.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Shared.Exceptions.Products
+{
+    public class ProductException : BaseException
+    {
+
+        public ProductException(int code, string message) : base(code, message)
+        {
+            
+        }
+
+        public static ProductException ProductNotFound()
+        {
+            return new ProductException(2001, "Product was not found.");
+        }
+
+    }
+}
