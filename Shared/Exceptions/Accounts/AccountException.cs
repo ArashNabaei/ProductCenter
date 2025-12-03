a7e6e6ca1853803a1b1a5d7d85e0942e5d346ac1
ArashNabaei <Arash_ANB@yahoo.com>
2025-12-03 08:09:27 +0330

    Create new base class for exceptions Add UserAlreadyExists exception

--- Shared/Exceptions/Accounts/AccountException.cs
+++ Shared/Exceptions/Accounts/AccountException.cs
@@ -1,7 +1,18 @@
 ﻿
 namespace Shared.Exceptions.Accounts
 {
-    public class AccountException
+    public class AccountException : BaseException
     {
+
+        public AccountException(int code, string message) : base(code, message)
+        {
+            
+        }
+
+        public static AccountException UserAlreadyExists()
+        {
+            return new AccountException(1001, "User already exists.");
+        }
+
     }
 }
---
+++ Shared/Exceptions/BaseException.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Shared.Exceptions
+{
+    public class BaseException : Exception
+    {
+
+        public int Code { get; set; }
+
+        protected BaseException(int code, string message) : base(message)
+        {
+            Code = code;
+        }
+
+    }
+}
